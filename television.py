8e239c589ad7a50e092a1626a14adf1562a1465c
Haase <78894109+LocoBunny04@users.noreply.github.com>
2023-11-20 11:58:47 -0600

    Update television.py

--- television.py
+++ television.py
@@ -1,14 +1,14 @@
 class Television:
-    __MIN_VOLUME = 0
-    __MAX_VOLUME = 2
-    __MIN_CHANNEL = 0
-    __MAX_CHANNEL = 3
+    MIN_VOLUME = 0
+    MAX_VOLUME = 2
+    MIN_CHANNEL = 0
+    MAX_CHANNEL = 3
 
     def __init__(self):
         self.__status = False
         self.__muted = False
-        self.__volume = Television.__MIN_VOLUME
-        self.__channel = Television.__MIN_CHANNEL
+        self.__volume = Television.MIN_VOLUME
+        self.__channel = Television.MIN_CHANNEL
 
     def power(self) -> bool:
         """
@@ -35,28 +35,28 @@ class Television:
         """If the status of the T.V is true, then the channel will increase. However, if the channel reaches
         max, it will go back to the minimum."""
         if self.__status:
-            if self.__channel < Television.__MAX_CHANNEL:
+            if self.__channel < Television.MAX_CHANNEL:
                 self.__channel += 1
                 # print(self.__channel)
-            elif self.__channel == Television.__MAX_CHANNEL:
-                self.__channel = Television.__MIN_CHANNEL
+            elif self.__channel == Television.MAX_CHANNEL:
+                self.__channel = Television.MIN_CHANNEL
 
     def channel_down(self: int):
         """If the status of the T.V is true, then the channel will decrease. However, if the channel reaches
         its minimum, it will go back to the max."""
 
         if self.__status:
-            if self.__channel > Television.__MIN_CHANNEL:
+            if self.__channel > Television.MIN_CHANNEL:
                 self.__channel -= 1
                 # print(self.__channel)
-            elif self.__channel == Television.__MIN_CHANNEL:
-                self.__channel = self.__MAX_CHANNEL
+            elif self.__channel == Television.MIN_CHANNEL:
+                self.__channel = self.MAX_CHANNEL
 
     def volume_up(self: int):
         """Checks to see the status of the muted function. If its muted, it will unmute and increase the value of the volume.
                 if the volume is already at maximum level then nothing changes."""
         if self.__status:
-            if self.__volume < Television.__MAX_VOLUME:
+            if self.__volume < Television.MAX_VOLUME:
                 if self.__muted:
                     self.__muted = False
                 self.__volume += 1
@@ -68,7 +68,7 @@ class Television:
         """Checks to see the status of the muted function. If its muted, it will unmute and decrease the value of the volume.
         if the volume is already at minimum then nothing changes."""
         if self.__status:
-            if self.__volume > Television.__MIN_VOLUME:
+            if self.__volume > Television.MIN_VOLUME:
                 if self.__muted:
                     self.__muted = False
                 self.__volume -= 1
